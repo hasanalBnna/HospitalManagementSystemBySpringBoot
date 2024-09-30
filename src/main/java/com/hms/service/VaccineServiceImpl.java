301631436cbbcd40458e87d7840304191b694d4b
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2024-09-30 12:54:54 +0600

    New Model added

--- src/main/java/com/hms/service/VaccineServiceImpl.java
+++ src/main/java/com/hms/service/VaccineServiceImpl.java
@@ -1,14 +1,14 @@
 package com.hms.service;
 
-import com.hms.model.User;
 import com.hms.model.Vaccine;
 import com.hms.repository.VaccineRepo;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Objects;
 
+@Service
 public class VaccineServiceImpl implements VaccineService{
 
 

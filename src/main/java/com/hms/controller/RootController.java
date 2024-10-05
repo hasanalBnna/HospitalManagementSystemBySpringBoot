f17f9bfdd367eabc6ddaa75a2b76524a4b74a5b4
Kazi Hasan Al Banna <130343033+hasanalBnna@users.noreply.github.com>
2024-10-05 12:09:38 +0600

    Update RootController.java

--- src/main/java/com/hms/controller/RootController.java
+++ src/main/java/com/hms/controller/RootController.java
@@ -8,20 +8,7 @@ public class RootController {
     @GetMapping(value="/")
     public String rootMap(){
 
-        return "Hello Checker, This is Banna. U here for check Vaccine module./users --->for get user info
-/users/add --->for add data to user
-/users/update --->for update data to user
-/users/delete --->for delete data to user
-
-/vaccine --->for get vaccine info
-/vaccine/add --->for add data to vaccine
-/vaccine/update --->for update data to vaccine
-/vaccine/delete --->for delete data to vaccine
-
-/vaccinationRecords --->for get vaccinationRecords info
-/vaccinationRecords/add --->for add data to vaccinationRecords
-/vaccinationRecords/update --->for update data to vaccinationRecords
-/vaccinationRecords/delete --->for delete data to vaccinationRecords";
+        return "Hello Checker, This is Banna. U here for check Vaccine module.";
     }
 
     @RequestMapping(value="/test", method= RequestMethod.GET)

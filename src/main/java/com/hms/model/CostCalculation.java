3e43dd345110020ce505e39323edce8cdd69bca9
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2024-10-06 18:50:23 +0600

    few delete

--- src/main/java/com/hms/model/CostCalculation.java
+++
@@ -1,6 +0,0 @@
-package com.hms.model;
-
-public interface CostCalculation {
-    int discountPercentage = 25;
-    public abstract double CalCost();
-}
--- src/main/java/com/hms/repository/HealthcareProvidersRepo.java
+++
@@ -1,7 +0,0 @@
-package com.hms.repository;
-
-import com.hms.model.HealthcareProviders;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HealthcareProvidersRepo extends JpaRepository<HealthcareProviders, Integer> {
-}
